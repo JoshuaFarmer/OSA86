2d6c2d15d77ecb9a7e5d2cc3f9b88eda42f36b91
Joshua Farmer <rhubarbjosh@outlook.com>
2025-04-01 17:37:12 +0100

    just because idk

--- src/boot.asm
+++ src/boot.asm
@@ -4,14 +4,42 @@
         KERNEL_SIZE  equ 128
         KERNEL_STACK equ 0xF000
         jmp _start
+        nop
+	sector_bytes: dw 512
+	cluster_sects: db 0
+	reserved_bpb: dw 0
+	FATs: db 2
+	roots: dw 0
+	total_sects: dw 5
+	media: db 0x80
+	FAT_sects: dw 0
+	track_sects: dw 18
+	head_cnt: dw 2
+	hidden_sects: dd 0
+	large_sects: dd 0
+	sects_per_32: dd 0
+	flags: dw 0
+	fat_v: dw 0
+	root_clust: dd 0
+	fsinfo: dw 0
+	backup: dw 0
+	reserved_e: dd 0 
+	dd 0 
+	dd 0 
+	drive_num: db 0x80
+	nt_flags: db 0
+	sig: db 0x28
+	volume: dd 0
+	volume_name: db "OSA86"
+	sys: db "FAT32 "
 _start:
-	push dx
 	cli
 	xor ax, ax
 	mov ss, ax
 	mov ds, ax
 	mov es, ax
 	mov sp, 0x7C00
+	push dx
 	sti
         ;mov ax,0x4F02
         ;mov bx,256
